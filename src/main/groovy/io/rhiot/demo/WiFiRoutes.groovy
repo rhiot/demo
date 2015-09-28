6dd0b300ded45bdbc07de7f5291571795deda0a2
Henryk Konsek <hekonsek@gmail.com>
2015-09-28 17:39:31 +0200

    Added REST configuration.

--- src/main/groovy/io/rhiot/demo/WiFiRoutes.groovy
+++ src/main/groovy/io/rhiot/demo/WiFiRoutes.groovy
@@ -1,14 +1,18 @@
 package io.rhiot.demo
 
+import io.rhiot.steroids.camel.Route
 import org.apache.camel.builder.RouteBuilder
 
 import static org.apache.camel.model.dataformat.JsonLibrary.Jackson
 
+@Route
 class WiFiRoutes extends RouteBuilder {
 
     @Override
     void configure() {
-        rest('/wifi').route().to('kura-wifi:*/*').marshal().json(Jackson)
+        restConfiguration().component("netty4-http").host("0.0.0.0").port(8080);
+
+        rest('/wifi').get().route().to('kura-wifi:*/*').marshal().json(Jackson)
     }
 
 }
