d269fc15cae3701ce8610a74a7dc6f47e04d04bf
Henryk Konsek <hekonsek@gmail.com>
2015-12-21 22:01:51 +0100

    Enabled tests.

--- README.md
+++ README.md
@@ -6,5 +6,5 @@ This is demo of Rhiot project.
 
 Demo consist of two elements. Raspberry Pi gateway deployed into a field and data stream backend service deployed into a cloud.
  
-Gateway collects GPS and WiFi data and send those to the data stream service. Data stream service is responsible for 
+Gateway collects GPS data and send those to the data stream service. Data stream service is responsible for 
 storing the data for a further analysis.
--- src/test/groovy/io/rhiot/demo/gateway/DemoGatewayTest.groovy
+++ src/test/groovy/io/rhiot/demo/gateway/DemoGatewayTest.groovy
@@ -1,6 +1,5 @@
 package io.rhiot.demo.gateway
 
-import io.rhiot.component.gpsd.ClientGpsCoordinates
 import io.rhiot.gateway.test.GatewayTest
 import org.apache.camel.component.mock.MockEndpoint
 import org.junit.Ignore
@@ -18,22 +17,16 @@ class DemoGatewayTest extends GatewayTest {
     protected void doBefore() {
         setStringProperty('log_appender', 'STDOUT')
 
-        setStringProperty('gps_endpoint', 'seda:gps')
         setStringProperty("gps_store_directory", gpsCoordinatesStore.getAbsolutePath());
         setStringProperty('gps_cloudlet_endpoint', 'mock:gps')
     }
 
-    @Ignore('We need to mock Wifi enriching.')
     @Test
     void shouldCollectAndSendGpsCoordinates() {
         // Given
-        def coordinates = new ClientGpsCoordinates(new Date(), 10.0, 20.0)
         def dataStreamConsumerMock = camelContext().getEndpoint('mock:gps', MockEndpoint.class)
         dataStreamConsumerMock.reset()
         dataStreamConsumerMock.setExpectedMessageCount(1)
-
-        // When
-        camelContext().createProducerTemplate().sendBody('seda:gps', coordinates)
 
         // Then
         dataStreamConsumerMock.assertIsSatisfied()
